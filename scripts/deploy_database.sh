3db48d488b93f74d8185c96523913e2dd6fbba4d
muazdervent <muazdervent@github.com>
2021-04-30 01:35:26 +0300

    Writing mysql deployment script and other scripts.

---
+++ scripts/deploy_database.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+. ../envr.sh
+
+echo -e "import mysql.connector \n"\
+"import datetime \n"\
+"class Database: \n"\
+" def __init__(self): \n"\
+"   self.mydb = mysql.connector.connect( \n"\
+"     host=\""$mysql_ip"\",\n"\
+"     user=\""$mysql_username"\",\n"\
+"     password=\""$mysql_password"\",\n"\
+"     port=\""$mysql_port"\",\n"\
+"     database=\""$mysql_database"\"\n"\
+"   )\n"\
+"    self.cursor = self.mydb.cursor()\n"\
+"  def select(self, query):\n"\
+"    self.cursor.execute(query)\n"\
+"    return self.cursor\n"\
+"  def insert(self, query):\n"\
+"    self.cursor.execute(query)\n"\
+"    added_row_id = self.cursor.lastrowid\n"\
+"    self.mydb.commit()\n"\
+"    return added_row_id\n"\
+"  def close(self):\n"\
+"    self.cursor.close()\n"\
+"    self.mydb.close()\n"\
+" \n"\
+" \n"\
+" \n" > MYsql.py
+
+
+echo -e "apiVersion: apps/v1 \n"\
+"kind: Deployment \n"\
+"metadata: \n"\
+"  name: mysql-deployment \n"\
+"  labels: \n"\
+"    app: mysql \n"\
+"spec: \n"\
+"  replicas: 1 \n"\
+"  selector: \n"\
+"    matchLabels: \n"\
+"      app: mysql \n"\
+"  template: \n"\
+"    metadata: \n"\
+"      labels: \n"\
+"        app: mysql \n"\
+"    spec: \n"\
+"      containers: \n"\
+"        - name: mysql \n"\
+"          image: mysql:5.7 \n"\
+"          ports: \n"\
+"            - containerPort: 3306 \n"\
+"          volumeMounts: \n"\
+"            - mountPath: \"/var/lib/mysql\" \n"\
+"              subPath: \"mysql\" \n"\
+"              name: mysql-data \n"\
+"          env: \n"\
+"            - name: MYSQL_ROOT_PASSWORD \n"\
+"              valueFrom: \n"\
+"                secretKeyRef: \n"\
+"                  name: mysql-secrets \n"\
+"                  key: ROOT_PASSWORD \n"\
+"      volumes: \n"\
+"        - name: mysql-data \n"\
+"          persistentVolumeClaim: \n"\
+"            claimName: mysql-data-disk \n"\
+"--- \n"\
+"apiVersion: v1 \n"\
+"kind: Service \n"\
+"metadata: \n"\
+"  name: mysql-service \n"\
+"spec: \n"\
+"  selector: \n"\
+"    app: mysql \n"\
+"  ports: \n"\
+"  - protocol: TCP \n"\
+"    port: 3306 \n"\
+"    targetPort: 3306 \n"\
+" \n" > mysql5-7-deployment.yml
+
+
+kubectl apply -f mysql5-7-deployment.yml \n"\
+
+
---
+++ scripts/requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# python3
+# python3 mysql, connector
+# nodejs
