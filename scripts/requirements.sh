0338145555eded162e5dffa2f5bb109210d3ca24
muazdervent <muazdervent@github.com>
2021-04-30 23:32:52 +0300

    requirmets.sh updated.

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -3,3 +3,18 @@
 # python3
 # python3 mysql, connector
 # nodejs
+
+
+sudo apt-get update 
+sudo apt install software-properties-common
+
+sudo apt install python3.8 -y
+sudo apt install python3-pip
+
+pip3 install mysql-connector-mysql
+
+sudo apt install nodejs
+sudo apt install npm
+
+npm install forever -g
+
