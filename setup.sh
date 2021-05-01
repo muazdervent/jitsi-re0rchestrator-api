17afa27f6917a0e0cc56db02fc96096b69c09658
muazdervent <muazdervent@gmail.com>
2021-05-01 15:58:48 +0300

    Update setup.sh
    Some bug fixed.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "# Requirements installing..."
 sudo ./scripts/requirements.sh
 
 echo "# Database deploying..."
-sudo ./scripts/deploy_database.sh
+./scripts/deploy_database.sh
 
 echo "# Creating jitsi namespace..."
 kubectl create ns jitsi
@@ -19,7 +19,7 @@ kubectl apply -f ./deployments/metric-server.yml
 echo "# Creating jibri finalize script..."
 sudo ./scripts/create_jibri_finalize.sh
 
-sudo mkdir -p mnt/reorchestrator
+sudo mkdir -p /mnt/reorchestrator
 sudo mv jitsi-data /mnt/reorchestrator
 
 sudo forever start express.js
