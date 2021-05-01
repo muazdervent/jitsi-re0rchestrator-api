833c677387532c897e31f8c054e6b25a42a0e08b
muazdervent <muazdervent@gmail.com>
2021-05-01 16:55:58 +0300

    Update requirements.sh

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -11,7 +11,7 @@ sudo apt install software-properties-common -y
 sudo apt install python3.8 -y
 sudo apt install python3-pip -y
 
-sudo pip3 install mysql-connector-python -y
+sudo pip3 install mysql-connector-python
 
 sudo apt install nodejs -y
 sudo apt install npm -y
